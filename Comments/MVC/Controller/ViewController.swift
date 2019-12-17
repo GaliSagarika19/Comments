244de34297943f7483793be62d2c7a1965ce9908
Srikanth <Gali_Srikanth@msrlimited.com>
2019-12-18 00:32:41 +0530

    Completed Comments Projects

---
+++ Comments/MVC/Controller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Comments
+//
+//  Created by Gali Srikanth on 18/12/19.
+//  Copyright © 2019 Sagarika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        singleLineComment()
+        multiLineComment()
+    }
+    
+    func singleLineComment() {
+        // This is how we define single line comment
+    }
+    
+    func multiLineComment() {
+        /*
+         this is multi
+         line comment
+         */
+    }
+}
